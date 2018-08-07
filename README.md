9e70394c83922b4dbbd555050afc5941ec501a6f
Manuel C Solis <manuelsolis@Manuels-MacBook-Air.local>
2018-08-07 18:03:34 -0400

    buttons work, displays temp. Need to add weather

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Weather Api
+
+- Create an input box for user to type in zip code or city name to find weather for that location
+
+- Search weather API for location and return the current weather for 
--- public/main.js
+++ public/main.js
@@ -3,3 +3,41 @@ const main = () => {
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+
+
+
+const searchWeatherAPI = () => {
+  const userInput = document.querySelector('.input').value
+
+  console.log(userInput)
+
+  const weatherKey = 'd75481a15f615534ede744531d37163a'
+
+
+  const _url = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + weatherKey
+
+  fetch(_url)
+    .then(resp => {
+      if (resp.status === 200) {
+        console.log("response = ", resp)
+        return resp.json()
+      } else {
+        console.log("err", resp)
+      }
+    }).then(weatherData => {
+      console.log("search results = ", weatherData)
+      const parent = document.querySelector('.searchResults')
+      
+      const newListItem = document.createElement('li')
+      newListItem.textContent = weatherData.main.temp
+      parent.appendChild(newListItem)
+      
+      
+    })
+}
+
+
+
+
+  document.querySelector('button').addEventListener('click', searchWeatherAPI)
