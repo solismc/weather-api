e324254cf9fe3de728543d508ba1885718ec8cd0
Manuel C Solis <manuelsolis@Manuels-MacBook-Air.local>
2018-08-10 13:59:21 -0400

    Refactored with classes and it WORKS!! yay!!

--- public/main.js
+++ public/main.js
@@ -9,35 +9,44 @@ document.addEventListener('DOMContentLoaded', main)
 
 const searchWeatherAPI = () => {
   const userInput = document.querySelector('.input').value
-
   console.log(userInput)
-
-  const weatherKey = 'd75481a15f615534ede744531d37163a'
-
-
-  const _url = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + weatherKey
-
-  fetch(_url)
-    .then(resp => {
-      if (resp.status === 200) {
-        console.log("response = ", resp)
-        return resp.json()
-      } else {
-        console.log("err", resp)
-      }
-    }).then(weatherData => {
-      console.log("search results = ", weatherData)
-      const parent = document.querySelector('.searchResults')
-      
-      const newListItem = document.createElement('li')
-      newListItem.textContent = weatherData.main.temp
-      parent.appendChild(newListItem)
-      
-      
-    })
 }
 
 
+const searchAPIEvent = () => {
+  const userInput = document.querySelector('.input').value
+  const apiSearch = new Search(userInput)
+  apiSearch.getSearchResults()
+}
+
+class Search {
+  constructor(searchTerm) {
+    const weatherKey = 'd75481a15f615534ede744531d37163a'
+    this.searchTerm = searchTerm
+    this.searchUrl = `https://api.openweathermap.org/data/2.5/weather?q=` + searchTerm + '&appid=' + weatherKey + '&unit=imperial'
+  }
+
+  getSearchResults() {
+    fetch(this.searchUrl)
+      .then(resp => {
+        if (resp.status === 200) {
+          console.log("response = ", resp)
+          return resp.json()
+        } else {
+          console.log("err", resp)
+        }
+      }).then(weatherData => {
+        console.log("search results = ", weatherData)
+        const parent = document.querySelector('.searchResults')
+
+        const newListItem = document.createElement('li')
+        newListItem.textContent = weatherData.main.temp
+        parent.appendChild(newListItem)
 
 
-  document.querySelector('button').addEventListener('click', searchWeatherAPI)
+      })
+  }
+}
+
+document.querySelector('.search-by-city-button').addEventListener('click', searchAPIEvent)
+document.querySelector('.search-by-zip-button').addEventListener('click', searchAPIEvent)
